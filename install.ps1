acb679aed9ffa77465fc55163f98802804a580db
Neil Crow <crowneil@gmail.com>
2024-10-31 17:14:12 +0000

    Simplified profile installation

--- install.ps1
+++ install.ps1
@@ -1,35 +1,15 @@
-[string]$target
+Write-Output "Installing profile ..."
 
-function helpinfo
-{
-    Write-Output "Help:"
-    Write-Output "Expected usage:   .\install.ps1 target"
-    Write-Output "  target            Required: profile"
-    Write-Output ""
+$profileDir = Split-Path -Path $profile
+if ( !( Test-Path $profileDir -PathType Container ) ) {
+    Write-Host "Creating $profileDir"
+    $dir = mkdir $profileDir
 }
 
-function installProfile
-{
-    $profileDir = Split-Path -Path $profile
-    Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.PowerShell_profile.ps1"
-    Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.VSCode_profile.ps1"
+Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.PowerShell_profile.ps1"
+Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.VSCode_profile.ps1"
 
-    # If (Test-Path Alias:ktl) {Remove-Item Alias:ktl}
-    # & $profile
-    # . $profile
-    Write-Output "Next please run :>. `$profile"
-}
-
-if ( $($args.Count) -gt 0) {
-    $target    = $args[0].ToLower()
-}
-
-#Check that the target is not null
-if ([string]::IsNullOrEmpty($target)) {
-    helpinfo
-    throw "target is mandatory"
-}
-
-if ($target -eq "profile") {
-    installProfile
-}
+# If (Test-Path Alias:ktl) {Remove-Item Alias:ktl}
+# & $profile
+# . $profile
+Write-Output "Now please run :>. `$profile"
