036b6ff62d00ecaed4192d9310ef5aad93f13e5a
crowne <crow.neil@gmail.com>
2026-02-11 01:17:33 +0000

    Don't fail when POSH_THEMES_PATH is null

--- install.ps1
+++ install.ps1
@@ -9,7 +9,7 @@ if ( !( Test-Path $profileDir -PathType Container ) ) {
 Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.PowerShell_profile.ps1"
 Copy-Item .\ps\profile\profile.ps1 "${profileDir}\Microsoft.VSCode_profile.ps1"
 
-if ( ( Test-Path $Env:POSH_THEMES_PATH -PathType Container ) ) {
+if ( $Env:POSH_THEMES_PATH -and ( Test-Path $Env:POSH_THEMES_PATH -PathType Container ) ) {
     Copy-Item -Force ./ps/oh-my-posh/git-on-up.omp.json $Env:POSH_THEMES_PATH
 }
 
